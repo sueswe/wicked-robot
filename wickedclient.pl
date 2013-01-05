a9bf31cb078c083f5a363fa700e3ab20ae392a57
Werner Suess <suess_w@gmx.net>
2013-01-05 19:50:21 +0100

    client uses now input

--- wickedclient.pl
+++ wickedclient.pl
@@ -10,13 +10,18 @@ use warnings;
 use strict;
 use IO::Socket;
 
+my $message = $ARGV[0];
+
+if (! defined $message) { print "No message given.\n"; exit(1); }
+
+
 my $sock = new IO::Socket::INET (
     PeerAddr => 'localhost',
     PeerPort => '7070',
     Proto => 'tcp',
     Type => SOCK_STREAM,
 ) || die "Could not create socket: $!\n";  #unless $sock;
-print $sock "deploy test";
+print $sock "$message";
 close($sock);
 
 
